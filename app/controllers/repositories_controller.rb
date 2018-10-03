64557078de774dbc5f6f92ca8382e047d6a6c052
Veronica Bustabad <vbustabad@gmail.com>
2018-10-03 17:10:17 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,11 +1,11 @@
 class RepositoriesController < ApplicationController
   def index
-    @service = GithubService.new
+    @service = GithubService.new({"access_token" => session[:token]})
     @repos_array = @service.get_repos
   end
 
   def create
-    @service = GithubService.new
+    @service = GithubService.new({"access_token" => session[:token]})
     @service.create_repo(params[:name])
     redirect_to '/'
   end
